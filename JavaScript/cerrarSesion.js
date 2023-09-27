6588f4970226ea9f01005cc9bb4efbe43ff3a46d
KRaf22 <rafagonzalezknappe@gmail.com>
2023-09-27 19:15:02 -0300

    Fetch a api/validate para validar que tipo de usuario inicio sesion, si no es funcionario redirigir a index

--- JavaScript/cerrarSesion.js
+++ JavaScript/cerrarSesion.js
@@ -1,32 +1,54 @@
 document.addEventListener("DOMContentLoaded", function () {
 
     const token = localStorage.getItem("access_token");
-    if (token==null){
-        location.href="index.html";
+    if (token == null) {
+        location.href = "index.html";
+    } else {
+        const headers = {
+            "Authorization": "Bearer " + token
+        };
+
+        fetch("http://localhost:8000/api/validate", {
+            method: "GET",
+            headers: headers
+        })
+            .then((response) => {
+                if (response.ok) {
+                    return response.json(); 
+                } 
+            })
+            .then((userData) => {
+                if (userData.tipo != "funcionario") {
+                    localStorage.removeItem("access_token");
+                    location.href = "index.html";
+                } 
+            })
+            .catch((error) => {
+                console.error("Error de red: ", error);
+            });
     }
 
     const cerrarSesion = document.getElementById("logOut");
-    
-    cerrarSesion.addEventListener("click", () => {
 
+    cerrarSesion.addEventListener("click", () => {
         const headers = {
-            "Authorization": "Bearer " + token
+            "Authorization": "Bearer " + token 
         };
 
         fetch("http://localhost:8000/api/logout", {
             method: "GET",
             headers: headers
         })
-        .then((response) => {
-            if (response.ok) {
-                localStorage.removeItem("access_token");
-                location.href = "index.html";
-            } else {
-                console.error("Error en la solicitud de cierre de sesión.");
-            }
-        })
-        .catch((error) => {
-            console.error("Error de red: ", error);
-        });
+            .then((response) => {
+                if (response.ok) {
+                    localStorage.removeItem("access_token");
+                    location.href = "index.html";
+                } else {
+                    console.error("Error en la solicitud de cierre de sesión.");
+                }
+            })
+            .catch((error) => {
+                console.error("Error de red: ", error);
+            });
     });
 });
