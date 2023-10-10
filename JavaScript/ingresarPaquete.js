8e8b036671bb1145f0566559692e2029f2c25051
KRaf22 <rafagonzalezknappe@gmail.com>
2023-10-10 00:30:47 -0300

    Conexion a Api-Almacenes para poder almacenar un producto en un almacen

--- JavaScript/ingresarPaquete.js
+++ JavaScript/ingresarPaquete.js
@@ -1,20 +1,26 @@
 document.addEventListener("DOMContentLoaded", function () {
 
     const token = localStorage.getItem("access_token");
-  
-    const urlIngresarPaquete = "http://localhost:8001/api/productos";
-  
-    const headers = {
-      "Authorization": "Bearer " + token,
-      "Content-Type": "application/json"
-    };
 
-    const formulario = document.getElementById("ingresarPaquete");
+    const headers = {
+        "Authorization": "Bearer " + token,
+        "Content-Type": "application/json"
+      };
+
+    const urlIngresarPaquete = "http://localhost:8001/api/productos";
+    const urlAlmacenamiento = "http://localhost:8001/api/almacena";
+
+    if (sessionStorage.getItem("productoId") != null) {
+        document.getElementById('id_producto').value=sessionStorage.getItem("productoId");
+    }
+
+    const formularioPaquete = document.getElementById("ingresarPaquete");
+    const formularioAlmacenamiento = document.getElementById("almacenamiento");
     const calle = document.getElementById("Calle");
     const numero_puerta = document.getElementById("NumeroPuerta");
     const forma_entrega = document.getElementById("FormaDeEntrega");
 
-    formulario.addEventListener("submit", function (event) {
+    formularioPaquete.addEventListener("submit", function (event) {
         event.preventDefault(); 
 
         let remitente= document.getElementById("Remitente").value;
@@ -55,7 +61,38 @@ document.addEventListener("DOMContentLoaded", function () {
         })
         .then(async response => {
           if (response.ok) {
-            console.log("Producto creado con éxito.");
+            const data = await response.json();
+            sessionStorage.setItem("productoId", data.id);
+            location.reload();
+          } else {
+            const errorMessage = await response.text(); 
+            console.error("Error en la solicitud:", errorMessage);
+          }
+        })
+        .catch(error => {
+          console.error("Error:", error);
+        });
+    });
+
+    formularioAlmacenamiento.addEventListener("submit", function (event) {
+        event.preventDefault(); 
+
+        let producto_id= document.getElementById("id_producto").value;
+        let almacen_id= document.getElementById("id_almacen").value;
+        
+        let data= {
+            "producto_id": producto_id,
+            "almacen_id": almacen_id,
+        };
+
+        fetch(urlAlmacenamiento, {
+            method: "POST",
+            headers: headers,
+            body: JSON.stringify(data)
+        })
+        .then(async response => {
+          if (response.ok) {
+            sessionStorage.removeItem("productoId");
             location.reload();
           } else {
             const errorMessage = await response.text(); 
