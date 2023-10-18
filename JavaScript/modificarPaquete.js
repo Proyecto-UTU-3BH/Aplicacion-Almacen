05d061e4dfeb174f612c3dc3540cd3d17d628e0e
KRaf22 <rafagonzalezknappe@gmail.com>
2023-10-18 18:49:18 -0300

    Api fetch a API-Almacen para modificar productos

--- JavaScript/modificarPaquete.js
+++ JavaScript/modificarPaquete.js
@@ -1,6 +1,7 @@
 function mostrarDatosPaquete (paquete) {
     document.getElementById("Remitente").value = paquete.remitente;
     document.getElementById("NombreDestinatario").value = paquete.nombre_destinatario;
+    document.getElementById("Estado").value = paquete.estado;
     document.getElementById("Peso").value = paquete.peso;
     document.getElementById("Destino").value = paquete.destino;
     document.getElementById("FormaDeEntrega").value = paquete.forma_entrega;
@@ -16,6 +17,7 @@ document.addEventListener("DOMContentLoaded", function () {
     const urlInfoPaquete = "http://localhost:8001/api/productos/"+idPaquete;
     const headers = {
         "Authorization": "Bearer " + token,
+        "Content-Type": "application/json"
     };
 
     fetch(urlInfoPaquete, {
@@ -37,5 +39,68 @@ document.addEventListener("DOMContentLoaded", function () {
         .catch((error) => {
           console.error("Error:", error);
         });
+
+        const formularioPaquete = document.getElementById("modificarPaquete");
+        const urlModificarPaquete = "http://localhost:8001/api/productos/"+idPaquete;
+        const calle = document.getElementById("Calle");
+        const numero_puerta = document.getElementById("NumeroPuerta");
+        const forma_entrega = document.getElementById("FormaDeEntrega");
+    
+        formularioPaquete.addEventListener("submit", function (event) {
+            event.preventDefault(); 
+    
+            let remitente= document.getElementById("Remitente").value;
+            let nombre_destinatario= document.getElementById("NombreDestinatario").value;
+            let peso= document.getElementById("Peso").value;
+            let destino= document.getElementById("Destino").value;
+            let tipo = document.getElementById("Tipo").value;
+            let estado=document.getElementById("Estado").value;
+    
+            if (forma_entrega.value === "reparto") {
+                if (calle.value.trim() === "" || numero_puerta.value.trim() === "") {
+                    Swal.fire({
+                        title: 'Campos faltantes',
+                        text: 'Por favor complete los campos de "Calle" y "Número de puerta" para el reparto.',
+                        icon: 'warning',
+                        confirmButtonText: 'Entendido'
+                    });
+                    return; 
+                }
+            }
+            
+            let info= {
+                "remitente": remitente,
+                "destino": destino,
+                "tipo": tipo,
+                "nombre_destinatario": nombre_destinatario,
+                "peso": peso,
+                "calle": calle.value,
+                "numero_puerta": numero_puerta.value,
+                "forma_entrega": forma_entrega.value,
+                "estado": estado
+            };
+    
+            fetch(urlModificarPaquete, {
+                method: "POST",
+                headers: headers,
+                body: JSON.stringify(info)
+            })
+            .then(async response => {
+              if (response.ok) {
+                const data = await response.json();
+                location.reload();
+              } else {
+                const errorMessage = await response.text(); 
+                console.error("Error en la solicitud:", errorMessage);
+              }
+            })
+            .catch(error => {
+              console.error("Error:", error);
+            });
+        });
+        
+        document.getElementById('redirigirButton').addEventListener('click', function(){
+            location.href="homepageAlmacen.html";
+        })
 });
 
